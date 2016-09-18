5b68e14908dc96b2c8325344f9ce2187637e3e1a
cameronfr <catechboy@hotmail.com>
2016-09-18 17:35:28 +0900

    renamed package from 'search' to 'frontend'

--- search/static/js/parser.js
+++
@@ -1,3 +0,0 @@
-//TODO: allow the browser to do it's own parsing, from say wikipedia (has X-domain API access)
-//Also post.message (see MDN) for in browser scraping
-//Components Here: {Scraping,Parsing}
